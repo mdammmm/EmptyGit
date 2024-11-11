eabf394daa74e790e1d7230379045e1f4c0035c7
mdammmm <101309928+mdammmm@users.noreply.github.com>
2024-11-11 21:17:20 +0900

    Исправлены недостатки кода в ветке patch1

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 
 def main():
-    name = input("Введите ваше имя: ") 
-    print(f"Hello world from {name}")  
+    name = input("Введите ваше имя: ")  
+    print(f"Hello world from {name}") 
 
 if __name__ == "__main__":
     main()
